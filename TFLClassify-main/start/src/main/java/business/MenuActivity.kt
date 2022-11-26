cd414f1d269b6d3a7f048dcf2284d52302cf6599
Vermes Balázs <qerix8787@gmail.com>
2022-11-27 00:35:44 +0100

    activities added

---
+++ TFLClassify-main/start/src/main/java/business/MenuActivity.kt
@@ -0,0 +1,30 @@
+package business
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.tensorflow.lite.examples.classification.databinding.ActivityMenuBinding
+
+
+class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btndetector.setOnClickListener {
+            val detectorIntent = Intent(this, MainActivity::class.java)
+            startActivity(detectorIntent)
+        }
+
+        binding.btnbreed.setOnClickListener {
+            val breedIntent = Intent(this, BreedCheckActivity::class.java)
+            startActivity(breedIntent)
+        }
+    }
+
+
+}
